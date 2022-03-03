f35c1064394af982280f91927604fcb46b0ae60f
Santiago <santtiago49@gmail.com>
2022-03-03 08:50:58 -0300

    Formulario reactivo tradicional

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
@@ -6,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class AgregarComponent implements OnInit {
+export class AgregarComponent {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', Validators.required ]
+  })
 
-  ngOnInit(): void {
+  constructor( private fb: FormBuilder) { }
+
+  tieneError(campo: string):boolean {
+    return this.miFormulario.get(campo)?.invalid || false;
   }
+
 
 }
