59739d2dded9d1451ceef5d64e4000fd21a3e009
Santiago <santtiago49@gmail.com>
2022-03-04 10:28:52 -0300

    Directivas estructurales personalizadas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { ErrorMsgDirective } from './directives/error-msg.directive';
+import { CustomIfDirective } from './directives/custom-if.directive';
 
 
 // Se declara y se exporta el ErrorMsgDirective para que otros modulos puedan
 // hacer uso de esa directiva
 @NgModule({
   declarations: [
-    ErrorMsgDirective
+    ErrorMsgDirective,
+    CustomIfDirective
   ],
   exports: [
-    ErrorMsgDirective
+    ErrorMsgDirective,
+    CustomIfDirective
   ]
 })
 export class SharedModule { }
